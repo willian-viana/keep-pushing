835246890c9e8bb885654ea7c6155fcd0f075bab
Willian Viana <willian.batista.viana@gmail.com>
2024-10-08 14:37:14 -0300

    feat(pwa): add next-pwa

--- packages/frontend/next.config.mjs
+++ packages/frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV != 'development'
+  }
+};
+
+export default withPWA({
+  dest: "public",         // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
+  register: true,         // register the PWA service worker
+  skipWaiting: true,      // skip waiting for service worker activation
+})(nextConfig)
